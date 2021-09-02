ebf5b516c2ecc6bf8b89f3bccc01150ddb87b8fa
Hasansattar <hasansattar650@gmail.com>
2021-09-03 02:47:36 +0500

    update

--- assigment4/NFT-721/contracts/NFT721.sol
+++ assigment4/NFT-721/contracts/NFT721.sol
@@ -11,12 +11,37 @@ pragma solidity ^0.8.0;
     uint public salePrice;
     uint private LAST_TOKEN_ID = 100;
     uint lastPublickSaleTokenIndex = 0;
+    uint public currentTime;
+    uint public saleStartDate;
+    uint public saleEndDate;
     
     constructor(uint _salePrice,
     string memory _tokenName,
     string memory _tokenSymbol) ERC721(_tokenName,_tokenSymbol){
         salePrice = _salePrice;
+        currentTime=block.timestamp;
     }
+
+     modifier isSaleOn() {
+        require(block.timestamp >= saleStartDate && block.timestamp <= saleEndDate, "Sorry, not a Sale period");
+        _;
+    }
+    
+       function saleStartAndSaleEnd() public  onlyOwner returns(uint256,uint256){
+         saleStartDate=  block.timestamp;
+         saleEndDate=  saleStartDate +  30 days;
+        return (saleStartDate,saleEndDate);
+           
+       }
+       
+
+
+    function setPrice(uint256 _setPrice)external  onlyOwner returns(uint256){
+          salePrice=_setPrice;
+          return salePrice;
+          
+      }
+    
     function _baseURI() internal view override returns(string memory){
         return baseURI;
     }
@@ -31,6 +56,7 @@ pragma solidity ^0.8.0;
     }
     
     function buyNft(uint quantity) external payable {
+         require(totalSupply<=100,"total supply should not increased 100");
         uint lastTokenId = lastPublickSaleTokenIndex;
         require(msg.value == salePrice * quantity,"Invalid amount!");
         
