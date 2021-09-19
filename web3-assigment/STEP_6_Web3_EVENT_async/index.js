74bf57e54a6307275bba40db7646f833fe354865
Hasansattar <hasansattar650@gmail.com>
2021-09-20 02:41:09 +0500

    STEP_6 Web3 EVENT async-await

--- web3-assigment/STEP_6_Web3_EVENT_async/index.js
+++ web3-assigment/STEP_6_Web3_EVENT_async/index.js
@@ -4,68 +4,65 @@ require("dotenv").config();
 
 rpcURL = `https://rinkeby.infura.io/v3/${process.env.INFURA_URL}`;
 const web3 = new Web3(rpcURL);
- 
+
 contract_Address = "0x64e4017FfbB2DDd7cef06eD3b7d583493008Cb12";
 
-
 const ABI = [
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "NumberCall",
-		"type": "event"
-	},
-	{
-		"inputs": [],
-		"name": "retrieve",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "num",
-				"type": "uint256"
-			}
-		],
-		"name": "store",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-] 
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "NumberCall",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "retrieve",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "num",
+        type: "uint256",
+      },
+    ],
+    name: "store",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
 const contractMethodAsync = async () => {
   try {
     const contract = new web3.eth.Contract(ABI, contract_Address);
-    let getAllEvent = await contract.getPastEvents("AllEvents",{
-      fromBlock:0,
-      toBlock:"latest"
+    let getAllEvent = await contract.getPastEvents("AllEvents", {
+      fromBlock: 0,
+      toBlock: "latest",
     });
     console.log("get All Events", getAllEvent);
-
-     
   } catch (error) {
     console.log("error", error);
   }
