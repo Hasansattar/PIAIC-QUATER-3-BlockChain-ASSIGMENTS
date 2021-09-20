d73f0977fbf1bf7613ae0f1d82f6ac26b4ddb193
Hasansattar <hasansattar650@gmail.com>
2021-09-21 01:10:42 +0500

    STEP_8 Web3 utilities

--- web3-assigment/STEP_8_Web3_Utilities/index.js
+++ web3-assigment/STEP_8_Web3_Utilities/index.js
@@ -8,25 +8,22 @@ const web3 = new Web3(rpcURL);
 const utilitiesAsync = async () => {
   try {
     // Get average gas price in wei from last few blocks median gas price
-   const getGasPrice =await web3.eth.getGasPrice();
-   console.log("gas price in wei",getGasPrice);
-   const result = await web3.utils.fromWei(getGasPrice, 'ether')
-   console.log("gas price in ether",result);
+    const getGasPrice = await web3.eth.getGasPrice();
+    console.log("gas price in wei", getGasPrice);
+    const result = await web3.utils.fromWei(getGasPrice, "ether");
+    console.log("gas price in ether", result);
 
-// Use sha256 Hashing function
-   const shaHash= await web3.utils.sha3('Dapp University');
-   console.log("get sha256 hash",shaHash)
+    // Use sha256 Hashing function
+    const shaHash = await web3.utils.sha3("Dapp University");
+    console.log("get sha256 hash", shaHash);
 
-// Use keccak256 Hashing function (alias)
-   const keccak256= await web3.utils.keccak256('Dapp University');
-   console.log("get keccak256 hash",keccak256)
+    // Use keccak256 Hashing function (alias)
+    const keccak256 = await web3.utils.keccak256("Dapp University");
+    console.log("get keccak256 hash", keccak256);
 
-// Get a Random Hex
-   const randomHex=await web3.utils.randomHex(32);
-   console.log("random hex", randomHex);
-
-   // Get access to the underscore JS library
-
+    // Get a Random Hex
+    const randomHex = await web3.utils.randomHex(32);
+    console.log("random hex", randomHex);
   } catch (error) {
     console.log("error", error);
   }
